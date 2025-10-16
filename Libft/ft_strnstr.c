b6ed70d63b0c371fc3d4c543ad593476ee355c62
mehdimillet <mehdi.millet@icloud.com>
2025-10-16 16:34:03 +0200

    Add files via upload
    ajout de strnstr

---
+++ Libft/ft_strnstr.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t i;
+	size_t j;
+	
+	i = 0;
+	j = 0;
+	if (needle[0] == 0)
+		return ((char *) haystack);
+	while (haystack[i] && i < len)
+	{
+		while (haystack[i + j] == needle[j] && haystack[i + j] && i + j < len)
+		{
+			j++;
+			if (needle[j] == 0)
+				return ((char *) haystack + i);
+		}
+		i++;
+		j = 0;
+	}
+	return (0);
+}
+
+int    main(void)
+{
+	char    str[] = "pasaimer";
+	char    to_find[] = "aimer";
+	printf("%s", ft_strnstr(str, to_find, 8));
+	return (0);
+}
